9cbafe8cc7f39e57eff73b53e4dfcd8a64c57dad
Vitalii <polarus35@gmail.com>
2023-03-30 19:56:12 +0200

    Added class "Program" and Interface "IDispatcherPresentationLayer"

--- L7 group/Program.cs
+++ L7 group/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using L7_group;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IDBItem_DataAccessLayer dbItem_DataAccessLayer = new DBItem_DataAccessLayer();
+        IDispatcherPresentationLayer dispatcherPresentationLayer = new DispatcherPresentationLayer(dbItem_DataAccessLayer);
+        MainMenu mainMenu = new MainMenu(dispatcherPresentationLayer);
+        DispatcherMenu dispatcherMenu = new DispatcherMenu(dispatcherPresentationLayer);
+        mainMenu.Show();
+    }
+}
