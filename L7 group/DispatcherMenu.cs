3b06aa54d492370433baefd77dd083a85b3e6511
Vitalii <polarus35@gmail.com>
2023-03-30 19:51:34 +0200

    Added class "DispatcherMenu"

---
+++ L7 group/DispatcherMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_group
+{
+    public class DispatcherMenu
+    {
+        private IDispatcherPresentationLayer dispatcherPresentationLayer;
+        public DispatcherMenu(IDispatcherPresentationLayer dispatcherPresentationLayer)
+        {
+            this.dispatcherPresentationLayer = dispatcherPresentationLayer;
+        }
+
+        public void Show()
+        {
+            while (true)
+            {
+                dispatcherPresentationLayer.ShowDispatcherMenu();
+            }
+        }
+    }
+}
