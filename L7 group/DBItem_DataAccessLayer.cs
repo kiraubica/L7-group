adfca2e18057aabc7554e4be450ea63464d28543
kinde <kinde@DESKTOP-O2QTT0D>
2023-03-30 20:29:52 +0300

    added class DBItem_DataAccessLayer

---
+++ L7 group/DBItem_DataAccessLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_group
+{
+    public class DBItem_DataAccessLayer : IDBItem_DataAccessLayer
+    {
+        private List<Flight> flights = new List<Flight>();
+
+        public void AddFlight(Flight flight)
+        {
+            flights.Add(flight);
+        }
+
+        public Flight GetFlightById(int id)
+        {
+            return flights.Find(flight => flight.Id == id);
+        }
+
+        public void RemoveFlight(Flight flight)
+        {
+            flights.Remove(flight);
+        }
+
+        public void UpdateFlight(Flight oldFlight, Flight newFlight)
+        {
+            int index = flights.IndexOf(oldFlight);
+            flights[index] = newFlight;
+        }
+    }
+}
