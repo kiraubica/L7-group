d74111e81287574b59d15a7c67e66483a2c07455
kinde <kinde@DESKTOP-O2QTT0D>
2023-03-30 20:18:28 +0300

    added Flight class

---
+++ L7 group/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_group
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public string Destination { get; set; }
+        public string? PointOfDeparture { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
