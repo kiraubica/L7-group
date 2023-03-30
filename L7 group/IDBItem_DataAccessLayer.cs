969865755e7a9f26f76c4a79f732c36a591ea402
ртивмыли <BaklanovNM@gmail.com>
2023-03-30 20:37:44 +0300

    Додано інтерфейс IDBItem_DataAccessLayer

---
+++ L7 group/IDBItem_DataAccessLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_group
+{
+
+    public interface IDBItem_DataAccessLayer
+    {
+        void AddFlight(Flight flight);
+        Flight GetFlightById(int id);
+        void RemoveFlight(Flight flight);
+        void UpdateFlight(Flight oldFlight, Flight newFlight);
+
+
+    }
+}
